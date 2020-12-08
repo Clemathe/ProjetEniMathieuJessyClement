86f64005ccadc6d72393647b2762dbfe7906cdbe
MathieuChenoun <chenoun.mathieu@gmail.com>
2020-12-08 11:13:59 +0100

    Pojo ArticleVendu et Categorie

--- src/fr/eni/encheres/bo/ArticleVendu.java
+++ src/fr/eni/encheres/bo/ArticleVendu.java
@@ -17,6 +17,119 @@ public class ArticleVendu {
 	private Utilisateur vendeur; 
 	private List<Enchere> encheres; 
 	private Categorie categorie; 
-	private Retrait lieuRetrait; 
+	private Retrait lieuRetrait;
+	
+	public ArticleVendu() {
+	}
+	
+	
+	public ArticleVendu(int noArticle, String nomArticle, String description, LocalDate dateDebutEncheres,
+			LocalDate dateFinEncheres, int miseAPrix, int prixVente, boolean etatVente, Utilisateur acheteur,
+			Utilisateur vendeur, List<Enchere> encheres, Categorie categorie, Retrait lieuRetrait) {
+		super();
+		this.noArticle = noArticle;
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		PrixVente = prixVente;
+		this.etatVente = etatVente;
+		this.acheteur = acheteur;
+		this.vendeur = vendeur;
+		this.encheres = encheres;
+		this.categorie = categorie;
+		this.lieuRetrait = lieuRetrait;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ArticleVendu [noArticle=" + noArticle + ", nomArticle=" + nomArticle + ", description=" + description
+				+ ", dateDebutEncheres=" + dateDebutEncheres + ", dateFinEncheres=" + dateFinEncheres + ", miseAPrix="
+				+ miseAPrix + ", PrixVente=" + PrixVente + ", etatVente=" + etatVente + ", acheteur=" + acheteur
+				+ ", vendeur=" + vendeur + ", encheres=" + encheres + ", categorie=" + categorie + ", lieuRetrait="
+				+ lieuRetrait + "]";
+	}
+
+
+	public int getNoArticle() {
+		return noArticle;
+	}
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+	public String getNomArticle() {
+		return nomArticle;
+	}
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public LocalDate getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+	public void setDateDebutEncheres(LocalDate dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+	public LocalDate getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+	public void setDateFinEncheres(LocalDate dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+	public int getMiseAPrix() {
+		return miseAPrix;
+	}
+	public void setMiseAPrix(int miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+	public int getPrixVente() {
+		return PrixVente;
+	}
+	public void setPrixVente(int prixVente) {
+		PrixVente = prixVente;
+	}
+	public boolean isEtatVente() {
+		return etatVente;
+	}
+	public void setEtatVente(boolean etatVente) {
+		this.etatVente = etatVente;
+	}
+	public Utilisateur getAcheteur() {
+		return acheteur;
+	}
+	public void setAcheteur(Utilisateur acheteur) {
+		this.acheteur = acheteur;
+	}
+	public Utilisateur getVendeur() {
+		return vendeur;
+	}
+	public void setVendeur(Utilisateur vendeur) {
+		this.vendeur = vendeur;
+	}
+	public List<Enchere> getEncheres() {
+		return encheres;
+	}
+	public void setEncheres(List<Enchere> encheres) {
+		this.encheres = encheres;
+	}
+	public Categorie getCategorie() {
+		return categorie;
+	}
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+	public Retrait getLieuRetrait() {
+		return lieuRetrait;
+	}
+	public void setLieuRetrait(Retrait lieuRetrait) {
+		this.lieuRetrait = lieuRetrait;
+	} 
 
 }
