7142010beefb94ed3c1559e53062dfa103016c56
PC\Clement <clement.mathe@gmail.com>
2020-12-08 11:15:54 +0100

    Ajout Jstl Modif Enchere

--- src/fr/eni/encheres/bo/Enchere.java
+++ src/fr/eni/encheres/bo/Enchere.java
@@ -8,5 +8,87 @@ public class Enchere {
 	private int montantEnchere; 
 	private ArticleVendu article; 
 	private Utilisateur utilisateur;
+	
+	/**
+	 * 
+	 */
+	public Enchere() {
+	}
 
+	/**
+	 * @param dateEnchere
+	 * @param montantEnchere
+	 * @param article
+	 * @param utilisateur
+	 */
+	public Enchere(LocalDate dateEnchere, int montantEnchere, ArticleVendu article, Utilisateur utilisateur) {
+		this.dateEnchere = dateEnchere;
+		this.montantEnchere = montantEnchere;
+		this.article = article;
+		this.utilisateur = utilisateur;
+	}
+
+	/**
+	 * @return the dateEnchere
+	 */
+	public LocalDate getDateEnchere() {
+		return dateEnchere;
+	}
+
+	/**
+	 * @param dateEnchere the dateEnchere to set
+	 */
+	public void setDateEnchere(LocalDate dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+
+	/**
+	 * @return the montantEnchere
+	 */
+	public int getMontantEnchere() {
+		return montantEnchere;
+	}
+
+	/**
+	 * @param montantEnchere the montantEnchere to set
+	 */
+	public void setMontantEnchere(int montantEnchere) {
+		this.montantEnchere = montantEnchere;
+	}
+
+	/**
+	 * @return the article
+	 */
+	public ArticleVendu getArticle() {
+		return article;
+	}
+
+	/**
+	 * @param article the article to set
+	 */
+	public void setArticle(ArticleVendu article) {
+		this.article = article;
+	}
+
+	/**
+	 * @return the utilisateur
+	 */
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+	/**
+	 * @param utilisateur the utilisateur to set
+	 */
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+
+	@Override
+	public String toString() {
+		return "Enchere [dateEnchere=" + dateEnchere + ", montantEnchere=" + montantEnchere + ", article=" + article
+				+ ", utilisateur=" + utilisateur + "]";
+	}
+
+	
 }
