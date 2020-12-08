0b686662f55df32f9c2016169640a50a0b712b8c
PC\Clement <clement.mathe@gmail.com>
2020-12-08 18:40:08 +0100

    U UtilisateurDAO, UtilisateurDAOJDBCImpl, UtilisateurManager,ServletConnexion

--- src/fr/eni/encheres/bll/UtilisateurDAO.java
+++ src/fr/eni/encheres/bll/UtilisateurDAO.java
@@ -4,6 +4,11 @@ import fr.eni.encheres.bo.Utilisateur;
 
 public interface UtilisateurDAO {
 	
-	public Utilisateur getUtilisateurByIdentifiant();
+	public Utilisateur getUtilisateurById();
+	
+	public boolean getExistingUser(String id);
+		
+	public  boolean getMatchingUserPassword(String id, String password);
 
 }
+
--- src/fr/eni/encheres/bll/UtilisateurManager.java
+++ src/fr/eni/encheres/bll/UtilisateurManager.java
@@ -1,7 +1,6 @@
 package fr.eni.encheres.bll;
 
-import java.util.List;
-
+import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.DAOFactory;
 
 
@@ -9,11 +8,30 @@ public class UtilisateurManager {
 
 	private static UtilisateurDAO utilisateurDAO;
 	
-	
 	/**
 	 * @param daoRepas
 	 */
 	public UtilisateurManager () {
 		utilisateurDAO = DAOFactory.getUtilisateurDAO();
 	}
+	
+	public Utilisateur utilisateurParIdentifiant() {
+		
+		return UtilisateurManager.utilisateurDAO.getUtilisateurById();
+		
+	}
+
+	public boolean utilisateurExistant(String id) {
+		boolean existingUser = UtilisateurManager.utilisateurDAO.getExistingUser(id);
+		return existingUser;
+		
+	}
+	
+	public boolean VerificationUtilisateurMotDePasse(String id, String password) {
+		boolean okUserPass = UtilisateurManager.utilisateurDAO.getMatchingUserPassword(id, password);
+		
+		return okUserPass;
+		
+	}
+	
 }
