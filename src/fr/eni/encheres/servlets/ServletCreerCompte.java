6e74cc3008c6554b2bd2d74440f263ff4e90bdaf
jmercer <jmercer@192.168.0.28>
2020-12-09 13:29:15 +0100

    U jsp/servlet Creation compte

--- src/fr/eni/encheres/servlets/ServletCreerCompte.java
+++ src/fr/eni/encheres/servlets/ServletCreerCompte.java
@@ -1,6 +1,8 @@
 package fr.eni.encheres.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +10,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.encheres.bo.Utilisateur;
 
 /**
  * Servlet implementation class ServletCreerCompte
@@ -22,6 +26,18 @@ public class ServletCreerCompte extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doG servletCreerCompte");
+		
+		request.setAttribute("pseudo", "pseudo");
+		request.setAttribute("prenom", "prenom");
+		request.setAttribute("nom", "nom");
+		request.setAttribute("email", "email");
+		request.setAttribute("telephone", "telephone");
+		request.setAttribute("rue", "rue");
+		request.setAttribute("codePostal", "codePostal");
+		request.setAttribute("ville", "ville");
+		request.setAttribute("motDePasse1", "motDePasse1");
+		request.setAttribute("motDePasse", "motDePasse");
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/PageCreerCompte.jsp"); 
 		rd.forward(request, response);
 	}
@@ -30,8 +46,40 @@ public class ServletCreerCompte extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println("doPost servletCreerCompte");
+		//Lecture des parametres 
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		// int noUtilisateur = 0; 
+		 String pseudo = null; 
+		 //String nom = null; 
+		 //String prenom = null; 
+		// String email = null; 
+		 //String telephone = null; 
+		 //String rue = null; 
+		 //String codePostal = null; 
+		 //String ville = null; 
+		 //String motDePasse = null; 
+		 //int credit = 0; 
+		 List<String> MessageErreur=new ArrayList<>();
+		 Utilisateur utilisateur = new Utilisateur(); 
+		 System.out.println("1");
+		 // lecture et vérification pseudo
+		 
+		 try {	
+			 System.out.println("2");
+			pseudo = request.getParameter(pseudo); 
+			if (pseudo != null && pseudo.matches("\\p{Alnum}") ) {
+				utilisateur.setPseudo(pseudo);
+				System.out.println("pseudo entré : " + pseudo);			
+			} else MessageErreur.add("erreur de saisie du pseudonyme : null ou caratères interdit"); 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		 
+		 System.out.println("fin"); 
+		
 	}
 	
 	private void creerCompte(HttpServletRequest request, HttpServletResponse response) {
