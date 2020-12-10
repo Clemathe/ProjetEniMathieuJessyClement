6b3f3ce7bf897a0ade5430c50730869c2c56744c
jmercer <jmercer@192.168.0.28>
2020-12-10 11:00:16 +0100

    u servletCreerCompte + u creerCompte jsp

--- src/fr/eni/encheres/servlets/ServletCreerCompte.java
+++ src/fr/eni/encheres/servlets/ServletCreerCompte.java
@@ -19,15 +19,15 @@ import fr.eni.encheres.bo.Utilisateur;
 @WebServlet("/CreerCompte")
 public class ServletCreerCompte extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-  
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("doG servletCreerCompte");
-		
+
 		request.setAttribute("pseudo", "pseudo");
 		request.setAttribute("prenom", "prenom");
 		request.setAttribute("nom", "nom");
@@ -38,54 +38,202 @@ public class ServletCreerCompte extends HttpServlet {
 		request.setAttribute("ville", "ville");
 		request.setAttribute("motDePasse1", "motDePasse1");
 		request.setAttribute("motDePasse", "motDePasse");
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/PageCreerCompte.jsp"); 
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/PageCreerCompte.jsp");
 		rd.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("doPost servletCreerCompte");
-		//Lecture des parametres 
-		
+		// Lecture des parametres
+
 		request.setCharacterEncoding("UTF-8");
-		
-		// int noUtilisateur = 0; 
-		 String pseudo = null; 
-		 //String nom = null; 
-		 //String prenom = null; 
-		// String email = null; 
-		 //String telephone = null; 
-		 //String rue = null; 
-		 //String codePostal = null; 
-		 //String ville = null; 
-		 //String motDePasse = null; 
-		 //int credit = 0; 
-		 List<String> MessageErreur=new ArrayList<>();
-		 Utilisateur utilisateur = new Utilisateur(); 
-		 System.out.println("1");
-		 // lecture et vérification pseudo
-		 
-		 try {	
-			 System.out.println("2");
-			pseudo = request.getParameter(pseudo); 
-			if (pseudo != null && pseudo.matches("\\p{Alnum}") ) {
+
+		// int noUtilisateur = 0;
+		String pseudo = null;
+		String nom = null;
+		String prenom = null;
+		String email = null;
+		String telephone = null;
+		String rue = null;
+		String codePostal = null;
+		String ville = null;
+		String motDePasse = null;
+		// int credit = 0;
+		List<String> MessageErreur = new ArrayList<>();
+		Utilisateur utilisateur = new Utilisateur();
+
+		// lecture et vérification pseudo
+
+		try {
+
+			pseudo = request.getParameter("pseudo");
+			if (pseudo == null || !pseudo.matches("[a-zA-Z0-9]+")) {
+				MessageErreur.add("erreur de saisie du pseudonyme : null ou caractères interdit");
+				System.out.println("erreur pseudo");
+			} else
 				utilisateur.setPseudo(pseudo);
-				System.out.println("pseudo entré : " + pseudo);			
-			} else MessageErreur.add("erreur de saisie du pseudonyme : null ou caratères interdit"); 
+			System.out.println("pseudo entré : " + pseudo);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		 
-		 System.out.println("fin"); 
-		
+
+		// lecture et vérification nom
+		try {
+			nom = request.getParameter("nom");
+			if (nom == null || !nom.matches("^[a-zA-Z]*$")) {
+
+				MessageErreur.add("erreur de saisie du Nom : null ou caractère interdit");
+				System.out.println("erreur nom");
+
+			} else
+				utilisateur.setNom(nom);
+			System.out.println("nom entré : " + nom);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// lecture et vérification prénom
+
+		try {
+			prenom = request.getParameter("prenom");
+			if (prenom == null || !prenom.matches("^[a-zA-Z]*$")) {
+
+				MessageErreur.add("erreur de saisie du Prenom : null ou caractère interdit");
+				System.out.println("erreur prenom");
+
+			} else
+				utilisateur.setPrenom(prenom);
+			System.out.println("nom entré : " + prenom);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// lecture et vérification email
+
+		try {
+			email = request.getParameter("email");
+			if (email == null || !email.matches("^[\\w.-]+@[\\w.-]+\\.[a-z]{2,}$")) {
+
+				MessageErreur.add("erreur de saisie de l'email : null ou format interdit");
+				System.out.println("erreur email");
+
+			} else
+				utilisateur.setEmail(email);
+			System.out.println("email entré : " + email);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// lecture et vérification téléphone
+
+		try {
+			telephone = request.getParameter("telephone");
+			if (telephone == null || !telephone.matches("(0|(\\+33)|(0033))[1-9][0-9]{8}")) {
+
+				MessageErreur.add("erreur de saisie du numéro de téléphone : null ou format interdit");
+				System.out.println("erreur telephone");
+
+			} else
+				utilisateur.setTelephone(telephone);
+			System.out.println("telephone entré : " + telephone);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// lecture et vérification adresse
+
+		try {
+			rue = request.getParameter("rue");
+			if (rue == null || !rue.matches("[a-zA-Z0-9]+")) {
+
+				MessageErreur.add("erreur de saisie de la rue : null ou caractère interdit");
+				System.out.println("erreur rue");
+
+			} else
+				utilisateur.setRue(rue);
+			System.out.println("rue entrée : " + rue);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// lecture et vérification ville
+
+		try {
+			ville = request.getParameter("ville");
+			if (ville == null || !rue.matches("^[a-zA-Z]*$")) {
+
+				MessageErreur.add("erreur de saisie de la rue : null ou caractère interdit");
+				System.out.println("erreur rue");
+
+			} else
+				utilisateur.setRue(rue);
+			System.out.println("rue entrée : " + rue);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// lecture et vérification code Postal
+
+		try {
+			codePostal = request.getParameter("codePostal");
+			if (codePostal == null || !codePostal.matches("^[0-9]{5}$")) {
+
+				MessageErreur.add("erreur de saisie du code postal : null ou caractère interdit");
+				System.out.println("erreur code postal");
+
+			} else
+				utilisateur.setCodePostal(codePostal);
+			System.out.println("code Postal entrée : " + codePostal);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * lecture et vérification des mots de passe avec contrainte : - 8 caractères au
+		 * minimum - au moins une lettre (minuscule ou majuscule) - au moins un chiffre
+		 * - les caractères spéciaux sont autoriés
+		 */
+		String motDePasse1 = null;
+
+		try {
+			motDePasse1 = request.getParameter("motDePasse1");
+			if (motDePasse1 == null || !motDePasse1.matches("^(?=.{8,})(?=.*[A-Za-z])(?=.*[0-9].*")) {
+				MessageErreur.add(
+						"erreur de saisie du mot de passe : il doit contenir 8 caractères au minimum, au moins une lettre (minuscule ou majuscule), au moins un chiffre, les caractères spéciaux sont autoriés");
+				System.out.println("erreur saisie motDepasse1");
+
+			} else
+				motDePasse = request.getParameter("motDePasse");
+			if (motDePasse1 != motDePasse) {
+				MessageErreur
+						.add("erreur de confirmation du mot de passe : veuillez entrer de nouveau votre mot de passe");
+				System.out.println("erreur confirmation motDepasse");
+			} else
+				utilisateur.setMotDePasse(motDePasse);
+			;
+			System.out.println("Mot de passe entré : " + motDePasse);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("fin");
+
 	}
-	
+
 	private void creerCompte(HttpServletRequest request, HttpServletResponse response) {
-		// lecture et vérification des paramètres et  
-		
-		
+		// lecture et vérification des paramètres et
+
 	}
 
 }
