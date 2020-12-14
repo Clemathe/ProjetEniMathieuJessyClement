5aec3369f710c64f2d1d7fd355624d775686437a
jmercer <jmercer@192.168.0.28>
2020-12-14 18:54:14 +0100

    +jsp/servlet MonProfil U utilisateur = javaBean +Servlet ModifierProfil

--- src/fr/eni/encheres/servlets/ServletModifierProfil.java
+++ src/fr/eni/encheres/servlets/ServletModifierProfil.java
@@ -1,51 +1,40 @@
 package fr.eni.encheres.servlets;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import fr.eni.encheres.bo.Utilisateur;
-
 /**
- * Servlet implementation class ServletMonProfil
+ * Servlet implementation class ServletModifierProfil
  */
-@WebServlet("/MonProfil")
-public class ServletMonProfil extends HttpServlet {
+@WebServlet("/ModifierProfil")
+public class ServletModifierProfil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletModifierProfil() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		//
-		Utilisateur utilisateur = new Utilisateur(); 
-		utilisateur = (Utilisateur) request.getSession(true).getAttribute("utilisateurCourant");
-		
-		if(utilisateur == null) {
-			RequestDispatcher rd = request.getRequestDispatcher("/connexion"); 
-			rd.forward(request, response);
-			System.out.println("renvoyer vers page se connecter");
-		}else
-			
-		request.setAttribute("utilisateur", utilisateur);
-		
-		
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/PageMonProfil.jsp"); 
-		rd.forward(request, response);
-		
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("DoPost Servlet MonProfil");
+		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
